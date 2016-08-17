a36968f5f151ba21f1f7f20f8fb27fadf740364a
Zach Cox <zcox522@gmail.com>
2016-08-16 21:36:55 -0500

    add explicit type annotations

--- user-information-http-service/src/main/scala/infrastructure/http4s/package.scala
+++ user-information-http-service/src/main/scala/infrastructure/http4s/package.scala
@@ -1,11 +1,12 @@
 package infrastructure
 
 import domain.User
+import org.http4s.{EntityDecoder, EntityEncoder}
 import org.http4s.circe.CirceInstances
 import io.circe._
 import io.circe.generic.auto._
 
 package object http4s extends CirceInstances {
-  implicit val userEntityDecoder = jsonOf[User]
-  implicit val userEntityEncoder = jsonEncoderOf[User]
+  implicit val userEntityDecoder: EntityDecoder[User] = jsonOf[User]
+  implicit val userEntityEncoder: EntityEncoder[User] = jsonEncoderOf[User]
 }
